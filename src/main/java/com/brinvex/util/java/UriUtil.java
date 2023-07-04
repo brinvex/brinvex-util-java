5fb3f13ed0d31e919354fa82ce616fbe6551e319
Lukas <lr@brinvex.com>
2023-07-04 15:12:11 +0200

    feat: Added UriUtil

---
+++ src/main/java/com/brinvex/util/java/UriUtil.java
@@ -0,0 +1,26 @@
+package com.brinvex.util.java;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class UriUtil {
+
+    /**
+     * https://stackoverflow.com/a/9608008/1157213
+     */
+    public static String extractDomainName(String url) {
+        try {
+            URI uri = new URI(url);
+            String domain = uri.getHost();
+            if (domain.startsWith("www.")) {
+                return domain.substring(4);
+            }
+            if (domain.startsWith("www3.")) {
+                return domain.substring(5);
+            }
+            return domain;
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
