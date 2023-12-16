70cc47154523488fadfbca7f4672849b3f0cb301
Lukas <lr@brinvex.com>
2023-12-16 16:22:53 +0100

    feat: Add HttpUtil.extractDomainName

--- release.bat
+++ release.bat
@@ -3,7 +3,7 @@ set JAVA_HOME="C:\tools\java\jdk-21.0.1"
 set MVN_HOME="C:\tools\mvn\mvn-3.8.7"
 
 REM Dont forget to update version in README
-set new_version=1.22.0
+set new_version=1.23.0
 
 call %MVN_HOME%\bin\mvn clean package
 
