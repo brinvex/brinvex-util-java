79f9b9ff748940ce7a51fbd6304e757eebd6d72e
Lukas <lr@brinvex.com>
2024-02-23 22:35:38 +0100

    release: v1.30.0

--- release.bat
+++ release.bat
@@ -1,9 +1,9 @@
 
 set JAVA_HOME="C:\tools\java\jdk-21.0.1"
-set MVN_HOME="C:\tools\mvn\mvn-3.8.7"
+set MVN_HOME="C:\tools\mvn\mvn-3.9.4"
 
 REM Dont forget to update version in README
-set new_version=1.29.0
+set new_version=1.30.0
 
 call %MVN_HOME%\bin\mvn clean package
 
