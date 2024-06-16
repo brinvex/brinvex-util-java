19e999d5cbab3e9052ef1dbe4ae8671748d7aef3
Lukas <lr@brinvex.com>
2024-06-16 15:09:44 +0200

    release: v1.54.2

--- src/main/java/com/brinvex/util/java/LazyValue.java
+++ src/main/java/com/brinvex/util/java/LazyValue.java
@@ -4,49 +4,86 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
 
 /**
- * It's thread safe. If you do not need thread-safety, consider to use use {@link ThreadUnsafeLazyValue}
- * <p>
  * Inspired by <a href="https://www.baeldung.com/java-lambda-lazy-field-initialization">www.baeldung.com/java-lambda-lazy-field-initialization</a>
  */
-@SuppressWarnings("JavadocLinkAsPlainText")
-public final class LazyValue<T> implements Supplier<T> {
+public interface LazyValue<T> extends Supplier<T> {
 
-    private final Supplier<T> supplier;
-
-    private final AtomicReference<Object> storedValueRef = new AtomicReference<>();
-
-    private final Object nullObject = new Object();
-
-    public LazyValue(Supplier<T> supplier) {
-        this.supplier = supplier;
+    static <T> LazyValue<T> threadSafe(Supplier<T> supplier) {
+        return new ThreadSafeLazyValue<>(supplier);
     }
 
-    @Override
-    public T get() {
-        Object storedValue = storedValueRef.get();
-        if (storedValue == null) {
-            synchronized (storedValueRef) {
-                storedValue = storedValueRef.get();
-                if (storedValue == null) {
-                    T value = supplier.get();
-                    storedValueRef.set(value == null ? nullObject : value);
-                    return value;
-                } else {
-                    if (storedValue == nullObject) {
-                        return null;
+    static <T> LazyValue<T> nonThreadSafe(Supplier<T> supplier) {
+        return new NonThreadSafeLazyValue<>(supplier);
+    }
+
+    final class ThreadSafeLazyValue<T> implements LazyValue<T> {
+
+        private final Supplier<T> supplier;
+
+        private final AtomicReference<Object> storedValueRef = new AtomicReference<>();
+
+        private final Object nullObject = new Object();
+
+        public ThreadSafeLazyValue(Supplier<T> supplier) {
+            this.supplier = supplier;
+        }
+
+        @Override
+        public T get() {
+            Object storedValue = storedValueRef.get();
+            if (storedValue == null) {
+                synchronized (storedValueRef) {
+                    storedValue = storedValueRef.get();
+                    if (storedValue == null) {
+                        T value = supplier.get();
+                        storedValueRef.set(value == null ? nullObject : value);
+                        return value;
+                    } else {
+                        if (storedValue == nullObject) {
+                            return null;
+                        }
+                        @SuppressWarnings({"ReassignedVariable", "unchecked"})
+                        T typedStoredValue = (T) storedValue;
+                        return typedStoredValue;
                     }
-                    @SuppressWarnings({"ReassignedVariable", "unchecked"})
-                    T typedStoredValue = (T) storedValue;
-                    return typedStoredValue;
                 }
+            } else {
+                if (storedValue == nullObject) {
+                    return null;
+                }
+                @SuppressWarnings({"ReassignedVariable", "unchecked"})
+                T typedStoredValue = (T) storedValue;
+                return typedStoredValue;
             }
-        } else {
-            if (storedValue == nullObject) {
-                return null;
+        }
+    }
+
+    final class NonThreadSafeLazyValue<T> implements LazyValue<T> {
+
+        private final Supplier<T> supplier;
+
+        private Object storedValue = null;
+
+        private final Object nullObject = new Object();
+
+        public NonThreadSafeLazyValue(Supplier<T> supplier) {
+            this.supplier = supplier;
+        }
+
+        @Override
+        public T get() {
+            if (storedValue == null) {
+                T value = supplier.get();
+                storedValue = value == null ? nullObject : value;
+                return value;
+            } else {
+                if (storedValue == nullObject) {
+                    return null;
+                }
+                @SuppressWarnings({"unchecked"})
+                T typedStoredValue = (T) storedValue;
+                return typedStoredValue;
             }
-            @SuppressWarnings({"ReassignedVariable", "unchecked"})
-            T typedStoredValue = (T) storedValue;
-            return typedStoredValue;
         }
     }
 
--- src/test/java/com/brinvex/util/java/LazyValueTest.java
+++ src/test/java/com/brinvex/util/java/LazyValueTest.java
@@ -12,7 +12,54 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
+/*
+todo 1 - Avoid a (harmless) Warning: Java HotSpot(TM) 64-Bit Server VM warning: Sharing is only supported for boot loader classes because bootstrap classpath has been appended
+https://github.com/mockito/mockito/issues/2590
+https://github.com/mockito/mockito/issues/3111
+ */
+/*
+todo 1 - Upgrade Mockito (and ByteBuddy) to support Java version 21+
+*/
 public class LazyValueTest {
+
+    @EnabledForJreRange(max = JRE.JAVA_20)
+    @Test
+    public void whenCalledMultipleTimes_thenShouldBeCalledOnlyOnce() {
+        @SuppressWarnings("unchecked") Supplier<String> mockedExpensiveFunction = Mockito.mock(Supplier.class);
+        Mockito.when(mockedExpensiveFunction.get())
+                .thenReturn("ok");
+        LazyValue<String> testee = LazyValue.nonThreadSafe(mockedExpensiveFunction);
+        Mockito.verify(mockedExpensiveFunction, Mockito.never())
+                .get();
+        testee.get();
+        testee.get();
+        Mockito.verify(mockedExpensiveFunction, Mockito.times(1))
+                .get();
+    }
+
+    @EnabledForJreRange(max = JRE.JAVA_20)
+    @Test
+    public void whenCalledMultipleTimesConcurrently_thenShouldBeCalledMultipleTimes() throws InterruptedException {
+        @SuppressWarnings("unchecked") Supplier<String> mockedExpensiveFunction = Mockito.mock(Supplier.class);
+        Mockito.when(mockedExpensiveFunction.get())
+                .thenAnswer((Answer<?>) invocation -> {
+                    Thread.sleep(1000L);
+                    return "ok";
+                });
+        LazyValue<String> testee = LazyValue.nonThreadSafe(mockedExpensiveFunction);
+        Mockito.verify(mockedExpensiveFunction, Mockito.never())
+                .get();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        executorService.invokeAll(List.of(testee::get, testee::get));
+        executorService.shutdown();
+        if (!executorService.awaitTermination(5, TimeUnit.SECONDS)) {
+            executorService.shutdownNow();
+        }
+
+        Mockito.verify(mockedExpensiveFunction, Mockito.times(2))
+                .get();
+    }
 
     @EnabledForJreRange(max = JRE.JAVA_20)
     @Test
@@ -23,7 +70,7 @@ public class LazyValueTest {
                     Thread.sleep(1000L);
                     return "ok";
                 });
-        LazyValue<String> testee = new LazyValue<>(mockedExpensiveFunction);
+        LazyValue<String> testee = LazyValue.threadSafe(mockedExpensiveFunction);
         Mockito.verify(mockedExpensiveFunction, Mockito.never())
                 .get();
 
